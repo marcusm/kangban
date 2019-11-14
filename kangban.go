8538269de9e07117dc999744bb4081bdfca0a5d3
Marcus Martin <450265+marcusm@users.noreply.github.com>
2019-11-13 21:49:17 -0800

    Simple room

--- kangban.go
+++ kangban.go
@@ -2,15 +2,67 @@ package main
 
 import tl "github.com/JoelOtter/termloop"
 
+type Player struct {
+	*tl.Entity
+	prevX int
+	prevY int
+	level *tl.BaseLevel
+}
+
+func (player *Player) Draw(screen *tl.Screen) {
+	screenWidth, screenHeight := screen.Size()
+	x, y := player.Position()
+	player.level.SetOffset(screenWidth/2-x, screenHeight/2-y)
+	player.Entity.Draw(screen)
+}
+
+func (player *Player) Tick(event tl.Event) {
+	if event.Type == tl.EventKey { // Is it a keyboard event?
+		player.prevX, player.prevY = player.Position()
+		switch event.Key { // If so, switch on the pressed key.
+		case tl.KeyArrowRight:
+			player.SetPosition(player.prevX+1, player.prevY)
+		case tl.KeyArrowLeft:
+			player.SetPosition(player.prevX-1, player.prevY)
+		case tl.KeyArrowUp:
+			player.SetPosition(player.prevX, player.prevY-1)
+		case tl.KeyArrowDown:
+			player.SetPosition(player.prevX, player.prevY+1)
+		}
+	}
+}
+
+func (player *Player) Collide(collision tl.Physical) {
+	// Check if it's a Rectangle we're colliding with
+	if _, ok := collision.(*tl.Rectangle); ok {
+		player.SetPosition(player.prevX, player.prevY)
+	}
+}
+
 func main() {
 	game := tl.NewGame()
-	game.Start()
-
+	game.Screen().SetFps(30)
 	level := tl.NewBaseLevel(tl.Cell{
-		Bg: tl.ColorGreen,
-		Fg: tl.ColorBlack,
-		Ch: 'v',
+		Bg: tl.ColorBlack,
+		Fg: tl.ColorWhite,
+		Ch: '#',
 	})
 
-	level.AddEntity(tl.NewRectangle(10, 10, 50, 20, tl.ColorBlue))
+	e := tl.NewEntity(0, 0, 10, 12)
+	e.Fill(&tl.Cell{
+		Bg: tl.ColorBlack,
+		Ch: ' ',
+	})
+
+	player := Player{
+		Entity: tl.NewEntity(1, 1, 1, 1),
+		level:  level,
+	}
+
+	// Set the character at position (0, 0) on the entity.
+	player.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '@'})
+	level.AddEntity(e)
+	level.AddEntity(&player)
+	game.Screen().SetLevel(level)
+	game.Start()
 }
