8b6d3a12f04a718a09d94efe47fa278344a5f9a7
Marcus Martin <marcmar@microsoft.com>
2019-11-14 16:13:53 -0800

    Fix player

--- kangban.go
+++ kangban.go
@@ -139,43 +139,6 @@ func (player Player) Draw(screen *tl.Screen) {
 	player.Entity.Draw(screen)
 }
 
-type Player struct {
-	*tl.Entity
-	prevX int
-	prevY int
-	level *tl.BaseLevel
-}
-
-func (player *Player) Draw(screen *tl.Screen) {
-	screenWidth, screenHeight := screen.Size()
-	x, y := player.Position()
-	player.level.SetOffset(screenWidth/2-x, screenHeight/2-y)
-	player.Entity.Draw(screen)
-}
-
-func (player *Player) Tick(event tl.Event) {
-	if event.Type == tl.EventKey { // Is it a keyboard event?
-		player.prevX, player.prevY = player.Position()
-		switch event.Key { // If so, switch on the pressed key.
-		case tl.KeyArrowRight:
-			player.SetPosition(player.prevX+1, player.prevY)
-		case tl.KeyArrowLeft:
-			player.SetPosition(player.prevX-1, player.prevY)
-		case tl.KeyArrowUp:
-			player.SetPosition(player.prevX, player.prevY-1)
-		case tl.KeyArrowDown:
-			player.SetPosition(player.prevX, player.prevY+1)
-		}
-	}
-}
-
-func (player *Player) Collide(collision tl.Physical) {
-	// Check if it's a Rectangle we're colliding with
-	if _, ok := collision.(*tl.Rectangle); ok {
-		player.SetPosition(player.prevX, player.prevY)
-	}
-}
-
 func main() {
 	game := tl.NewGame()
 
